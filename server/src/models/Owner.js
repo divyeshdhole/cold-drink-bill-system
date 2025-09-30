b17044a877dd24a47156a1f6a3069b1cf3232f72
divyeshdhole <divyeshlic2003@gmail.com>
2025-09-30 23:37:36 +0530

    delete

---
+++ server/src/models/Owner.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+//change file name
+const OwnerSchema = new mongoose.Schema({
+  totalBills: { type: Number, default: 0 },
+  totalAmount: { type: Number, default: 0 },
+  totalReceived: { type: Number, default: 0 },
+  totalPending: { type: Number, default: 0 }
+}, { timestamps: true });
+
+export default mongoose.model("Owner", OwnerSchema);
